08ae75f0c42b4fc39001c6bc1345a350e75e613a
emanuelosva <emanuelosva@gmail.com>
2020-07-02 07:14:18 -0500

    Validate request payload by Joi

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,18 @@
 'use strict'
 
 const userController = require('../controllers/user');
+const validSchema = require('../schemas/user');
 
 module.exports = [
   // Create user by POST
   {
     method: 'POST',
     path: '/user/sing-in',
+    options: {
+      validate: {
+        payload: validSchema.userRegisterSchema,
+      }
+    },
     handler: userController.registerUser,
   },
 ];
