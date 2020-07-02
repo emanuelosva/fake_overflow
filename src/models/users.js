4205de715abf6e77fc4617f033243b8ad8d1a330
emanuelosva <emanuelosva@gmail.com>
2020-07-02 09:03:29 -0500

    Validate user data for login

--- src/models/users.js
+++ src/models/users.js
@@ -17,6 +17,27 @@ class Users {
 
     return newUser.key
   };
+
+  // Validate user on login
+  async validateUser(data) {
+    const userQuery = await this.collection
+      .orderByChild('email')
+      .equalTo(data.email)
+      .once('value')
+
+    const userFound = userQuery.val();
+    if (userFound) {
+      const userId = Object.keys(userFound)[0];
+      const correctPassword = await encrypt.
+        compare(data.password, userFound[userId].password);
+
+      const result = correctPassword ? userFound[userId] : false;
+
+      return result
+    }
+
+    return false;
+  };
 }
 
 module.exports = Users;
